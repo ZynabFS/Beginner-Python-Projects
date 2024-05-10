e51708c8566a663596b08e2b03207ad95348bf55
Berry <162787355+ZynabFS@users.noreply.github.com>
2024-05-10 09:41:36 +0500

    guessnumloop.py

--- Guess the Num (Loops)/GuessNum.py
+++ Guess the Num (Loops)/GuessNum.py
@@ -6,12 +6,12 @@ answer = False
 while not answer:
     guessNum = input("Enter a Guess Number: ") # writig the guessNum's input inside the loop to make the loop work.
     if int(guessNum) < 99:
-        print("Too Low!") # their guess was wrong.
+        print("Too Low!") # the guess was wrong.
 
     elif int(guessNum) > 99:
-        print("Too High!") # their guess was still wrong.
+        print("Too High!") # the guess was still wrong.
 
     else:
         print("Done")
-        answer = True  # we got the correct answer, so exit the loop.
+        answer = True  # the guess was the correct answer, so exit the loop.
      # if the answer was false, that means the while loop will continue.
